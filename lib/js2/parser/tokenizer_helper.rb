53762dc4a82a020d3844b034be4905926737c57a
Jeff Su <me@jeffsu.com>
2010-03-22 13:57:19 +0800

    Adding tokenizer

---
+++ lib/js2/parser/tokenizer_helper.rb
@@ -0,0 +1,24 @@
+class JS2::Parser::TokenizerHelper
+  def start_node (type)
+  end
+
+  def stop_node 
+  end
+
+  def start_line
+  end
+
+  def stop_line
+  end
+
+  private
+
+  def _start
+  end
+
+  def _stop
+  end
+
+
+
+end
