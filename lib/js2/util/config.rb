5ace72ad19dc83bc580836ba04dbc57b4c829c0a
Jeff Su <me@jeffsu.com>
2010-03-24 17:42:17 +0800

    Fixing js2 exececutable

--- lib/js2/util/config.rb
+++ lib/js2/util/config.rb
@@ -15,10 +15,33 @@ class JS2::Util::Config
     return self.from_hash(hash)
   end
 
-  def rails!
+  def load_hash (hash)
+    self.file_handler.js2_dir = hash['js2_dir'] if hash['js2_dir']
+    self.haml_engine = eval(hash['haml_engine_class'] + '.new') if hash['haml_engine_class']
+
+    write_dir = hash['write_dir'] || hash['out_dir']
+    self.file_handler.out_dir = write_dir if write_dir
+    self.file_handler.haml_dir = hash['haml_dir'] || config.file_handler.js2_dir
+  end
+
+  def load_yml (yml, env = nil)
+    hash = YAML.load_file('./config/js2.yml')  
+    hash = hash[env] if env
+    self.load_hash(hash)
+  end
+
+  def rails! (env = nil)
     self.out_dir = './public/javascript'
     self.js2_dir = './app/js2'
+
+    begin
+      if File.exist?('./config/js2.yml')
+        self.load_yml('./config/js2.yml', env)
+      end
+    rescue
+    end
   end
+  
 
   def out_dir= (dir)
     @file_handler.out_dir = dir
@@ -26,6 +49,18 @@ class JS2::Util::Config
 
   def js2_dir= (dir)
     @file_handler.js2_dir = dir
+  end
+
+  def haml_dir= (dir)
+    @file_handler.haml_dir = dir
+  end
+
+  def to_s
+    return <<-END
+js2_dir: #{@file_handler.js2_dir}
+out_dir: #{@file_handler.out_dir}
+haml_dir: #{@file_handler.haml_dir}
+    END
   end
 
   def from_yml (yml, env = nil)
@@ -36,15 +71,16 @@ class JS2::Util::Config
 
   def self.from_hash(hash)
     config = self.new
-
-    config.file_handler.js2_dir = hash['js2_dir'] if hash['js2_dir']
-    config.haml_engine = eval(hash['haml_engine_class'] + '.new') if hash['haml_engine_class']
-
-    write_dir = hash['write_dir'] || hash['out_dir']
-    config.file_handler.out_dir = write_dir if write_dir
-
-    config.file_handler.haml_dir = hash['haml_dir'] || config.file_handler.js2_dir
+    config.load_hash(hash)
 
     return config
+  end
+
+  def example_yml
+    return <<-END
+js2_dir:  './app/js2'      
+haml_dir: './app/js2'
+out_dir:  './public/javascripts'
+    END
   end
 end
