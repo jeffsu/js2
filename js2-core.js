194cc0f527fe46fb2af72861b2714889dd03da62
Jeff Su <me@jeffsu.com>
2011-02-18 23:50:56 -0800

    yay!

--- js2-core.js
+++ js2-core.js
@@ -3,7 +3,6 @@ var JS2 = require('./js2-class').JS2;
 var GenericContent = JS2.Class.extend({
   klass: "GenericContent",
   sanityShift: 20,
-  started: true,
   markerTokens: {
     'class' : 'Klass',
     'foreach' : 'Foreach'
@@ -14,6 +13,7 @@ var GenericContent = JS2.Class.extend({
     this.next    = null;
 
     if (tokenizer) {
+      this.line = tokenizer.line;
       this.tokenizer  = tokenizer;
       this.curlyCount = tokenizer.curlyCount; 
       this.braceCount = tokenizer.braceCount; 
@@ -26,6 +26,16 @@ var GenericContent = JS2.Class.extend({
 
   shift: function() {
     return this.content.shift();
+  },
+
+  collapse: function() {
+    var newContent = [];
+    for (var i=0; i<this.content.length; i++) {
+      var token = this.content[i];
+      if ((typeof token != 'string') || !token.match(/^\s/)) newContent.push(token);
+    }
+
+    this.content = newContent;
   },
 
   shiftOff: function(str) {
@@ -43,11 +53,10 @@ var GenericContent = JS2.Class.extend({
       if (this.next.closed) this.next = null;
     } 
 
-    else if (token in this.markerTokens && this.started) {
+    else if (token in this.markerTokens) {
       var klass = eval(this.markerTokens[token]);
       this.newContent(klass, token);
     } 
-
    
     else {
       this.addContent(token);    
@@ -55,17 +64,15 @@ var GenericContent = JS2.Class.extend({
   },
 
   newContent: function (klass, token) {
-    this.next = new klass(this.tokenizer);
+    var next = this.next = new klass(this.tokenizer);
     this.content.push(this.next);
     this.next.push(token);
     if (this.next.closed) this.next = null;
+    return next;
   },
 
   addContent: function(token) {
     this.content.push(token);
-    if (this.curlyCount > 0 && token == '}' && this.curlyMatch()) {
-      this.closed = true;
-    }
   },
 
   toString: function() {
@@ -81,27 +88,48 @@ var GenericContent = JS2.Class.extend({
   },
 
   braceMatch: function() {
-    return this.curlyCount-1 == this.tokenizer.curlyCount;
+    return this.braceCount-1 == this.tokenizer.braceCount;
   }
+});
 
+var Braces = GenericContent.extend({
+  klass: "Braces",
+  markerTokens: {},
+  addContent: function(token) {
+    this.content.push(token);
+    if (!this.started && token == '(') {
+      this.started = true;
+    } else if (this.started && token == ')' && this.braceMatch()) {
+      this.closed = true;
+    }
+  }
+});
 
+var Block = GenericContent.extend({
+  klass: "Block",
+  addContent: function(token) {
+    this.content.push(token);
+    if (!this.started && token == '{') {
+      this.started = true;
+    } else if (this.started && token == '}' && this.curlyMatch()) {
+      this.closed = true;
+    }
+  }
 });
 
 var Klass = GenericContent.extend({
   klass: "Klass",
   markerTokens: {},
   addContent: function(token) {
-    this.content.push(token);
-    if (this.inScope()) {
-      if (token == 'function') {
-        this.newContent(Method, this.content.pop());
-      } else if (token == 'var') {
-        this.newContent(Member, this.content.pop());
-      }
-    } else if (token == '{') {
-      this.started = true;
-    } else if (token == '}' && this.curlyMatch()) {
-      this.closed = true;
+    if (token == '{') {
+      var block = this.newContent(Block, token);
+      block.markerTokens = {
+        'var' : 'Member',
+        'function' : 'Method'
+      };
+      this.block = block;
+    } else {
+      this.content.push(token);
     }
   },
 
@@ -111,13 +139,12 @@ var Klass = GenericContent.extend({
   },
 
   toString: function() {
-    var klass = this.content.shift();
-    var uselessSpace = this.content.shift();
-    var name  = this.content.shift();
-    var space = this.content.shift();
-    var body  = this.super().replace(/,(\s*)$/, "$1");
+    this.collapse();
+    this.shift();
+    var name    = this.shift();
+    var content = this.block.toString().replace(/,(\s*\}\s*)$/, '$1');
 
-    return "var" + space + name + '=JS2.Class.extend(' + body + ');';
+    return "var " +  name + '=JS2.Class.extend(' + content + ');';
   },
 
 });
@@ -126,11 +153,12 @@ var Method = GenericContent.extend({
   klass: "Method",
   addContent: function(token) {
     if (token == '{') {
-      this.newContent(GenericContent, token);
+      this.block = this.newContent(Block, token);
     } else {
       this.content.push(token);
     } 
 
+    if (this.block && this.block.closed) this.closed = true;
   },
 
   toString: function() {
@@ -143,28 +171,56 @@ var Method = GenericContent.extend({
 });
 
 var Foreach = GenericContent.extend({
+  counter: 0,
   klass: "Foreach",
   markerTokens: {},
   addContent: function(token) {
-    this.content.push(token);
     if (token == '(') {
-      if (!this.started) {
-        this.started = true;
-      }
-    } else if (token == ')' && this.started && this.braceMatch()) {
-      this.braceClosed = true;
-    } else if (this.braceClosed && token == '{') {
-      console.log('hi');
-      this.addContent(GenericContent, this.content.pop());
-    }
+      this.braces = this.newContent(Braces, token); 
+    } 
 
-    if (this.next && this.next.closed) {
-      this.closed = true;
+    else if (token == '{') {
+      this.curlies = this.newContent(Block, token);
+    } 
+
+    else {
+      this.content.push(token);
+      if (this.curlies && this.curlies.closed) this.closed = true;
     }
   },
 
   toString: function () {
-    return "foreach";
+    this.counter++;
+
+    this.braces.collapse();
+    this.braces.shift();
+    this.braces.pop();
+
+    var holder = this.braces.shift();
+    var temp   = this.braces.shift();
+    var iterator = null;
+
+    if (temp == ':') {
+      iterator = this.braces.shift();
+    } else {
+      iterator = '_it' + this.counter;
+    }
+
+    this.braces.shift();
+
+    var len = '_len' + this.counter;
+    var rest = this.braces.content.join('');
+    var container = '_con' + this.counter;
+
+    var ret = "for (var " + iterator + "=0," + 
+               container + "=" + rest + "," + 
+               len + "=" + container + ".length," + holder + ";" + 
+               holder + "=" + container + '[' + iterator + ']||' + 
+               iterator + "<" + len + ";" + iterator + "++)" + 
+               this.curlies.toString();
+    
+    this.counter--;
+    return ret;
   }
 });
 
@@ -178,52 +234,60 @@ var Member = GenericContent.extend({
   },
 
   toString: function() {
-    this.shiftOff('var'); 
-    var space = this.shift();
-    var name  = this.shift();
-    this.shiftOff('=');
+    this.collapse();
+    this.shift(); 
+    var name = this.shift();
+    this.shift();
     this.pop();
 
     return '"' + name + '":' + this.super() + ',';
   }
-
 });
 
+var Parser = JS2.Class.extend({
+  initialize: function () {
+    this.js2_tokenizer = require('./js2-tokenizer').parser;
+    this.reset();
+    this.js2_tokenizer.yy = this;
+  },
 
-
-function Tokenizer () {
-  this.reset();
-  this.js2_tokenizer = require('./js2-tokenizer').parser;
-  this.js2_tokenizer.yy = this;
-}
-
-Tokenizer.prototype = {
   reset: function () {
     this.braceCount = 0;
     this.curlyCount = 0;
+    this.line = 0;
     this.root = new GenericContent(this);
   },
+
   parse: function(str) {
     this.js2_tokenizer.parse(str);
   },
   
   append: function (str) {
+    if (str == "\n") this.line++;
     this.root.push(str);
   },
 
   toString: function () {
     return this.root.toString();
   }
-};
+});
+
+Parser.parse = function (str) {
+  var parser = new this();
+  parser.parse(str);
+  return parser.root;
+}
 
 
-var tokenizer = new Tokenizer();
-tokenizer.parse("class FooBar {\n" +
+var test1 = "class FooBar {\n" +
 "  var foo = 'hello';\n " +
 "  function foobar() {\n " +
 "    console('hi');\n " +
 "    foreach (var i in j) {\n " +
 "    }\n " +
 "  }\n " +
-"}");
-console.log(tokenizer.toString());
+"}";
+var test2 = "foreach (var k in x) { foreach (var i in j) { hello } }";
+var root = Parser.parse(test1);
+console.log(root.toString());
+
