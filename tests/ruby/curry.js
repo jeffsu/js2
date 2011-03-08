43f154a75062945725028d62020bb06a3e6d0765
Jeff Su <me@jeffsu.com>
2011-03-08 17:00:30 +0800

    adding to curry test

--- tests/ruby/curry.js
+++ tests/ruby/curry.js
@@ -1,12 +1,17 @@
-var scope = {};
-(function(){return function($1,$2,$3){
+var scope = { foo: 'bar' };
+
+(function(scope){return function($1,$2,$3){
+
+}})(scope);
+
+(function(scope){return function(arg1){
 
 }})(scope);
 
 (function(){return function(arg1){
 
-}})(scope);
+}})();
 
-(function(){return function(arg1){
+(function($this){return function(arg1){
 
-}})()
+}})(this)
